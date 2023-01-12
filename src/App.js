bf98b1ebfe1f17960056787474591637398226a1
Tneswick Laptop <tneswick2@verizon.net>
2023-01-11 16:33:59 -0800

    raise search bar to top level

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import './App.css';
-import Header from './components/Header'
+import React, { useState } from 'react';
+import Body from './components/Body';
+// import Header from './components/Header'
 
 function App() {
+
+  const [inputText, setInputText] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleChange = (event) => {
+    setInputText({ ...inputText, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSearchTerm(inputText)
+    setInputText('')
+  };
+
+  // conditional render
+
+  const Header = () => {
+    return (
+      <div className="header-wrapper">
+        <h1 className="header-text">
+          🎞️🍿Full On Flicks🎬🎥
+        </h1>
+        <form onSubmit={handleSubmit}>
+          <input type="text" className="search" value={inputText} onChange={handleChange} placeholder="Search for Movies or TV Shows" />
+          <input type="submit" className="search-btn" />
+        </form>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <Header />
-      <header className="App-header">
-        <p>
-          HELLO
-        </p>
-      </header>
+      {/* <Header /> */}
+      {handleBody}
     </div>
   );
 }
